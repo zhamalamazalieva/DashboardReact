503061cfea7c5defd3cbfeac60b803cb10fab297
zhamalamazalieva <jamalamazalieva@gmail.com>
2021-01-05 16:02:33 +0100

    added chartjs and imported GroupedBar

--- src/pages/dashboard/chart.jsx
+++ src/pages/dashboard/chart.jsx
@@ -2,29 +2,19 @@ import React from 'react'
 import { Bar } from '@reactchartjs/react-chart.js'
 
 const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  labels: ['1', '2', '3', '4', '5', '6'],
   datasets: [
     {
-      label: '# of Votes',
+      label: '# of Red Votes',
       data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
-      ],
-      borderWidth: 1,
+      backgroundColor: 'rgb(255, 99, 132)',
     },
+    {
+      label: '# of Blue Votes',
+      data: [2, 3, 20, 5, 1, 4],
+      backgroundColor: 'rgb(54, 162, 235)',
+    },
+   
   ],
 }
 
@@ -40,21 +30,10 @@ const options = {
   },
 }
 
-const VerticalBar = () => (
+const GroupedBar = () => (
   <>
-    <div className='header'>
-      <h1 className='title'>Vertical Bar Chart</h1>
-      <div className='links'>
-        <a
-          className='btn btn-gh'
-          href='https://github.com/reactchartjs/react-chartjs-2/blob/react16/example/src/charts/VerticalBar.js'
-        >
-          Github Source
-        </a>
-      </div>
-    </div>
     <Bar data={data} options={options} />
   </>
 )
 
-export default VerticalBar;
+export default GroupedBar
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import TopCards from './topCards.jsx';
 import './style.scss';
-// import VerticalBar from './chart'
-
+import GroupedBar from './chart';
 const Dashboard = () => {
     return (
         <section className='dashboard'>
             <div className='section-container'>
                <TopCards />
-               {/* <VerticalBar /> */}
+                <div className='row'>
+                    <div className='col-9'>
+                    <GroupedBar />
+                    </div>
+                    <div className='col-3'></div>
+                </div>
             </div>
         </section>
     )
